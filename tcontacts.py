69070c77021564179a66c118de9d67b98051cc0c
antoniosirait <103513680+antoniosirait29@users.noreply.github.com>
2022-06-12 14:32:33 +0700

    pain

---
+++ tcontacts.py
@@ -0,0 +1,6 @@
+"""This module provides RP Contacts entry point script."""
+
+from tcontacts.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ tcontacts/database.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""This module provides a database connection."""
+
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+
+def _createContactsTable():
+    """Create the contacts table in the database."""
+    createTableQuery = QSqlQuery()
+    return createTableQuery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            phone VARCHAR(13) NOT NULL,
+            email VARCHAR(40)
+        )
+        """
+    )
+
+
+def createConnection(databaseName):
+    """Create and open a database connection."""
+    connection = QSqlDatabase.addDatabase("QSQLITE")
+    connection.setDatabaseName(databaseName)
+
+    if not connection.open():
+        QMessageBox.warning(
+            None,
+            "RP Contact",
+            f"Database Error: {connection.lastError().text()}",
+        )
+        return False
+
+    _createContactsTable()
+    return True
